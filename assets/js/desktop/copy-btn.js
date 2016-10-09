d24ab37dddb9fde837b3aeda7ded1746b7dcb2d4
cedmax <marco.cedaro@gmail.com>
2016-10-09 21:03:04 +0100

    first draft of the electron app

---
+++ assets/js/desktop/copy-btn.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default (props)=>{
+  var style;
+  if (props.hide){
+    style = {
+      visibility: 'hidden'
+    }
+  }
+  return <div style={style}><button className={props.service} data-clipboard-text={props.value}>
+    copy from {props.service}
+  </button></div>
+}

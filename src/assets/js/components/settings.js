a8cb981189536e3549754e653c00bd1d3e5cb8be
cedmax <marco.cedaro@gmail.com>
2016-10-17 08:10:44 +0100

    arrow, better settings close

--- src/assets/js/components/settings.js
+++ src/assets/js/components/settings.js
@@ -4,8 +4,6 @@ import List from 'material-ui/lib/lists/list';
 import ListItem from 'material-ui/lib/lists/list-item';
 import Subheader from 'material-ui/lib/Subheader';
 import Divider from 'material-ui/lib/Divider';
-import IconButton from 'material-ui/lib/icon-button';
-import Icon from 'material-ui/lib/svg-icons/navigation/close';
 import { saveSettings, quit, getSetting } from 'js/actions';
 
 export default class NavMenu extends React.Component {
@@ -26,13 +24,6 @@ export default class NavMenu extends React.Component {
     return (
       <List>
         <Subheader>
-          <div style={{float:'right'}}>
-          <IconButton
-            onClick={this.props.closeMenu}
-          >
-            <Icon />
-          </IconButton>
-          </div>
           Settings
         </Subheader>
         <ListItem
